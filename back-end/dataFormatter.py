28da1d3a4ef3508b43f10bf6994dc952adf447a9
Heet <hparik5@illinois.edu>
2022-11-19 18:40:18 -0600

    Adding dataFormatter.py

--- back-end/dataFormatter.py
+++ back-end/dataFormatter.py
@@ -1,50 +1,26 @@
-
-
-# // data: {
-# //     video1: [transcript1: ,
-# //              timestamp1: ,
-
-# //             transcript2: ,
-# //             timestamp2: ,]
-    
-# // }
-
-
-# //{"message":"0:06\r\neverything we're doing up in space
-# //        \r\n0:08\r\nis to benefit humanity or benefit human
-# //        \r\n0:10\r\nexploration
-# //        \r\n0:14\r\nwe are natural explorers we want to
-# //        \r\n0:16\r\nexplore our environment whatever small
-# //        \r\n0:18\r\narea we start in we always want to push
-# //        \r\n0:19\r\nthe boundaries
-# //        \r\n0:20\r\n[Music]
-# //        \r\n0:25\r\nthe great thing about space travel is"}
-
-# // Not sure why it is not working tho!
-# //Probably you are right! I don't it is there in JS!
-# // Something like the above example
-
-# // Parse it to JSON from a single string! Probably would be easier in python dictionary
-# //Can we use a python function?
-# Yeah 
-
-#Yeah Exactly!
-
-
+import json
 
 def dataFormatter(dataToSend):
     temp = dataToSend
-    mydict = {"timestamp": "transcript"}
-    idx = temp.find("\r\n")
-    timestamp = temp[0:idx]
-    print(timestamp)
-    temp.replace(temp[0:idx],"")
-    transcript = temp[0:temp.find("\r\n")]
-    print(transcript)
-    temp.replace(temp[0:temp.find("\r\n")],"")
+    mydict = {}
+    while temp != '\0':
+        timestamp = temp[0:temp.find("\r\n")]
+        temp = temp[temp.find("\r\n")+2:]
+        idx = temp.find("\r\n")
+        if(idx == -1):
+            idx = len(temp)
+            transcript = temp[0:idx]
+            temp = '\0'
+        else:
+            transcript = temp[0:idx]
+            temp = temp[temp.find("\r\n")+2:]
+        mydict[timestamp] = transcript
+    # print(mydict)
+    json_object = json.dumps(mydict, indent = 4) 
+    # print(json_object)
+    return(json_object)
+    
 
+print(dataFormatter("0:06\r\neverything we're doing up in space\r\n0:08\r\nis to benefit humanity or benefit human\r\n0:10\r\nexploration\r\n0:14\r\nwe are natural explorers we want to"))
 
-dataFormatter("0:06\r\neverything we're doing up in space\r\n0:08\r\nis to benefit humanity or benefit human")
-
-#Vrush...could you please run this script?
 
