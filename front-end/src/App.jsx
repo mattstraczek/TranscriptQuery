44d927ed3d6e20fa690747a28460304fd4033dc2
unknown <adnan.noorullah@gmail.com>
2022-11-23 14:23:59 -0600

    Updated fixes for input fields

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,11 +10,13 @@ function App() {
   const [ChannelName, setChannelName] = useState('');
   const [StartDate, setStartDate] = useState('');
   const [EndDate, setEndDate] = useState('');
+  const [SearchQuery, setSearchQuery] = useState('');
 
   const sendInfo = () => {
     console.log(ChannelName);
     console.log(StartDate);
     console.log(EndDate);
+    console.log(SearchQuery);
     Axios.post('http://localhost:3001/getQuery', {ChannelName: ChannelName, StartDate: StartDate, EndDate: EndDate}).then((response) => {
     });
   }
@@ -83,7 +85,7 @@ function App() {
 
         <input value = {EndDate} onChange={(event) => {setEndDate(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="End Date (ENTER YEAR)" required />
 
-        <input value = {EndDate} onChange={(event) => {setEndDate(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Query (ENTER PHRASE)" required />
+        <input value = {SearchQuery} onChange={(event) => {setSearchQuery(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Query (ENTER PHRASE)" required />
 
         <button onClick={sendInfo} class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
     </div>
