5447e35d301140f52f6e3179d9028971196a8e21
unknown <adnan.noorullah@gmail.com>
2022-12-05 22:42:03 -0600

    Finished frontend setup for search engine

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './shared/Header';
 import SearchEngine from './SearchEngine';
+import Main_Page from './main_page';
 
 function App() {
   const [ChannelName, setChannelName] = useState('');
@@ -19,7 +20,7 @@ function App() {
     console.log(StartDate);
     console.log(EndDate);
     console.log(SearchQuery);
-    <Link to='./search'></Link>
+    <Link to='./SearchEngine'></Link>
     Axios.post('http://localhost:3002/query', {ChannelName: ChannelName, StartDate: StartDate, EndDate: EndDate, SearchQuery: SearchQuery}).then((response) => {
     console.log(response).catch(err => console.log(err));
   });
@@ -61,54 +62,18 @@ function App() {
   // }
 
   return (
-    // <div className="App">
-    //   <input placeholder="Enter text"/>
-    //   {Data.filter(
-    //     post => {
-    //       if (query == '') {
-    //         return post;
-    //       } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
-    //         return post
-    //       }
-    //     }
-    //   ).map((post) => (
-    //     <div className="box" key={post.id}>
-    //       <p>{post.title}</p>
-    //       <p>{post.author}</p>
-    //     </div>
-    //   ))}
-    // </div>
-
-    <form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-    <View syle={styles.container}><Text style={styles.setFontSize}>YouTube Transcript Search</Text></View>
-    <View syle={styles.container}><Text style={styles1.setFontSize}>Adnan Noorullah - Vrush Patel - Heet Parikh - Matt Straczek - Anshul Goswami</Text></View>
-    <div class="relative">
-        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input value = {ChannelName} onChange={(event) => {setChannelName(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Channel Name" required />
-
-        <input value = {StartDate} onChange={(event) => {setStartDate(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Start Date (ENTER YEAR)" required />
-
-        <input value = {EndDate} onChange={(event) => {setEndDate(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="End Date (ENTER YEAR)" required />
-
-        <input value = {SearchQuery} onChange={(event) => {setSearchQuery(event.target.value);}} type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Query (ENTER PHRASE)" required />
-
-        <button onClick={sendInfo} class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
     <div>
         <Router>
-          <div class="flex flex-col h-screen justify-between">
+          <div class="flex flex-col h-screen ">
             <Header />
             <Routes>
-                <Route exact path = '/' element = {<App />} />
-                <Route exact path = '/search' element = {<SearchEngine />} />
+                {/* <Route exact path = '/' element = {<App />} />  */}
+                <Route exact path = '/' element = {<Main_Page />} />
+                <Route exact path = '/SearchEngine' element = {<SearchEngine />} />
             </Routes>
           </div>
         </Router>
-    </div>
-</form>
+    </div> 
 
   );
 
